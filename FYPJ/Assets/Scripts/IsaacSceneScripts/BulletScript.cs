8a6e3ecdf65b38b33cee6e30c74cb36b3491e8af
Isaac Choo <isaacchoo_jl@hotmail.com>
2018-06-07 16:05:06 +0800

    Updating my scene and scripts

--- FYPJ/Assets/Scripts/IsaacSceneScripts/BulletScript.cs
+++ FYPJ/Assets/Scripts/IsaacSceneScripts/BulletScript.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_PS4
-using UnityEngine.PS4;
-#endif
 
 public class BulletScript : MonoBehaviour {
 
@@ -20,26 +17,17 @@ public class BulletScript : MonoBehaviour {
         bulletLifeTime -= Time.deltaTime;
         if (bulletLifeTime <=0)
         {
-            Destroy(this);
+            Destroy(gameObject);
         }
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    Destroy(this);
-    //    if (other.gameObject.tag == "Player")
-    //    {
-    //        StartCoroutine(Vibrate());
-    //    }
-    //}
-
-    //IEnumerator Vibrate()
-    //{
-
-    //    PS4Input.MoveSetVibration(0, 1, 128);
-    //    PS4Input.MoveSetVibration(0, 0, 128);
-    //    yield return new WaitForSeconds(0.1f);
-    //    PS4Input.MoveSetVibration(0, 1, 0);
-    //    PS4Input.MoveSetVibration(0, 0, 0);
-    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            VibrationScript vibrationScript = other.GetComponent<VibrationScript>();
+            vibrationScript.StartCoroutine(vibrationScript.Vibrate());
+        }
+        Destroy(gameObject);
+    }
 }
