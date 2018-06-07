e74ce5afd2e86b35c65358870a68717bb5727a5b
Isaac Choo <isaacchoo_jl@hotmail.com>
2018-06-07 15:51:37 +0800

    added lounge

--- FYPJ/Assets/Scripts/IsaacSceneScripts/BulletScript.cs
+++ FYPJ/Assets/Scripts/IsaacSceneScripts/BulletScript.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_PS4
+using UnityEngine.PS4;
+#endif
 
 public class BulletScript : MonoBehaviour {
 
@@ -19,5 +22,24 @@ public class BulletScript : MonoBehaviour {
         {
             Destroy(this);
         }
-	}
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Destroy(this);
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        StartCoroutine(Vibrate());
+    //    }
+    //}
+
+    //IEnumerator Vibrate()
+    //{
+
+    //    PS4Input.MoveSetVibration(0, 1, 128);
+    //    PS4Input.MoveSetVibration(0, 0, 128);
+    //    yield return new WaitForSeconds(0.1f);
+    //    PS4Input.MoveSetVibration(0, 1, 0);
+    //    PS4Input.MoveSetVibration(0, 0, 0);
+    //}
 }
