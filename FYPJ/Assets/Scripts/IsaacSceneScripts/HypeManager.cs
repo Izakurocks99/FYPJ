51c406ad81bc9912b9445a4db8a29fd54cb5336e
Isaac Choo <isaacchoo_jl@hotmail.com>
2018-06-08 15:09:07 +0800

    - demo scene done

---
+++ FYPJ/Assets/Scripts/IsaacSceneScripts/HypeManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HypeManager : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject audienceManager;
+
+    private float playerScore = 0;
+    private float scoreHypeRatio = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(audienceManager == null)
+        {
+            audienceManager = FindObjectOfType<AudianceManager>().gameObject;
+        }
+		if (playerScore != player.GetComponent<PlayerStats>()._intPlayerScoring)
+        {
+            playerScore = player.GetComponent<PlayerStats>()._intPlayerScoring;
+            audienceManager.GetComponent<AudianceManager>().HypeMeter = Mathf.RoundToInt(playerScore * scoreHypeRatio);
+        }
+	}
+}
