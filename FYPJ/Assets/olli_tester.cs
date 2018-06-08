5f97439046dea20b836a32d77f25ba3f9fbac506
Patrik Broms <patrik.broms95@gmail.com>
2018-06-08 14:30:16 +0800

    --

---
+++ FYPJ/Assets/olli_tester.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class olli_tester : MonoBehaviour {
+
+    [SerializeField]
+    Shader bloomShader;
+
+    Material bloom;
+
+	void OnRenderImage (RenderTexture source, RenderTexture destination) {
+
+        if (bloom == null) {
+			bloom = new Material(bloomShader);
+			bloom.hideFlags = HideFlags.HideAndDontSave;
+		}
+
+        int widht = source.width / 8;
+        int height = source.height / 8;
+        RenderTexture r = RenderTexture.GetTemporary(
+            widht, height, 0,source.format );  // 0 for indicating that we wont fo anything for depth buffer
+
+		Graphics.Blit(source,r, bloom);
+		Graphics.Blit(r, destination, bloom);
+        RenderTexture.ReleaseTemporary(r);
+
+
+
+
+        // shader stuff
+
+	}
+
+	//void Start () {
+		
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
