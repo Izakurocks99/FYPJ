55da045b70820ed61ae25547f6459f18ac83f8bb
Patrik Broms <patrik.broms95@gmail.com>
2018-06-12 16:25:12 +0800

    Skydome

---
+++ FYPJ/Assets/render_test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class render_test : MonoBehaviour {
+
+    Material material = null;
+    [SerializeField]
+    Shader sha = null;
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Debug.Assert(sha != null);
+        if (material == null)
+        {
+            material = new Material(sha);
+        }
+
+    }
+}
