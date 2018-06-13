8278e92c9c6184708bd1eb8d4700f9bae7e9463f
LYoshiiro <lyoshiiro@gmail.com>
2018-06-13 17:15:48 +0800

    Change to Debug codes names and Audio Beat Spawning.
    Changes:
     - Change the debug script's names and remove not needed stuff.
     - Change the Audio Beat spawning so they no longer spawn the same colors if they double spawn and they also no long spawn the two color of one light stick.
     - Changed the hierachcy so that the audio beats no longer changes shape due to parent object *however this script's changes need to make changes to the hiearchcy.*
    Additions:
     - Added the debu script for generation of Audio Scales so to reduce the need of having to recreate it everytime the scales need to be changed.

---
+++ FYPJ/Assets/Scripts/General Scripts/DebugCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCamera : MonoBehaviour {
+
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+            Camera.main.transform.eulerAngles -= new Vector3(0.0f, 80.0f, 0.0f) * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+            Camera.main.transform.eulerAngles += new Vector3(0.0f, 80.0f, 0.0f) * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+            Camera.main.transform.eulerAngles -= new Vector3(80.0f, 0.0f, 0.0f) * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.S))
+            Camera.main.transform.eulerAngles += new Vector3(80.0f, 0.0f, 0.0f) * Time.deltaTime;
+    }
+}
