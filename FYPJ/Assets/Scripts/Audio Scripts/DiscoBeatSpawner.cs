201dfd6d49ef89a5e6f53a028ce04e58dafc3bef
LYoshiiro <lyoshiiro@gmail.com>
2018-06-21 17:03:33 +0800

    Major changes to disc motion and disc beat spawning
    Changes:
     - Change the pattern in how often the disco ball rotate.
     - Change the transformation to only discoball
     - Change to give the beats spawn from discoball to take in a original transformation that is from the disco ball's current position.
     - Change the patter in how the beat is spawned from the disco ball.
    Note:
     - Do not take in the speakers and turn off the scripts.

--- FYPJ/Assets/Scripts/Audio Scripts/DiscoBeatSpawner.cs
+++ FYPJ/Assets/Scripts/Audio Scripts/DiscoBeatSpawner.cs
@@ -10,43 +10,56 @@ public class DiscoBeatSpawner : MonoBehaviour {
 	public GameObject _goBandVisualiser;
 	float _ftTime;
 	float _ftWait;
+	float _ftXIncrement;
+	float _ftY;
 	int _intCount;
+	int _intCurrent;
 
 	void Start() {
-		_ftTime = 0;
-		_ftWait = 1.0f;
+		_ftTime = 0.0f;
+		_ftWait = 0.125f;
+		_ftXIncrement = 0.0f;
 		_intCount = 0;
+		_intCurrent = 0;
 	}
 
 	void Update () {
 		if (_intCount == 0)
-			_intCount = Random.Range(15, 40);
+			// _intCount = Random.Range(15, 35);
+			_intCount = 11;
+
+		_ftY = Mathf.Lerp(-0.25f, 0.25f, Mathf.PingPong(Time.time, 1));
 
 		if (_goAudio.GetComponent<AudioSource>().clip != null &&
 			_goAudio.GetComponent<AudioSource>().isPlaying == true)
-			GenerateDrag();
+			if (_goPlayer.GetComponent<PlayerStats>()._intSpawnPoint == 1)
+				GenerateDrag();
 	}
 
 	void GenerateDrag() {
-		if (_goPlayer.GetComponent<PlayerStats>()._intSpawnPoint == 1) {
-			
-			for (int i = 0; i < _intCount; i++)
-			{
-				if ((_ftTime += 1 * Time.deltaTime) >= _ftWait)
-				{
-					GameObject go = Instantiate(_goPrefab, this.transform, false);
-					go.name = "Test " + i;
-					go.SetActive(true);
-					_goPlayer.GetComponent<PlayerStats>().listGO.Add(go);
-					_ftTime = 0;
-					_goBandVisualiser.GetComponent<AudioBandVisualiser>()._ftTime -= 0.1f;
-					if (i == _intCount - 1)
-					{
-						_goPlayer.GetComponent<PlayerStats>()._intSpawnPoint = 0;
-						_ftTime -= 2.0f;
-					}
-				}
+		if (_intCount % 2 == 1)
+			_ftXIncrement = 0.5f / (_intCount / 2);
+
+		if ((_ftTime += 1 * Time.deltaTime) >= _ftWait) {
+			if (_intCurrent < _intCount) {
+
+				GameObject go = Instantiate(_goPrefab, this.transform, false);
+				go.name = "Test " + _intCurrent;
+				go.SetActive(true);
+
+				go.transform.position = this.GetComponent<DiscoMotion>()._vec3Spawn;
+				float _ftX = _ftXIncrement * _intCurrent;
+				go.GetComponent<DiscoBeatMotion>()._vec3Shift = new Vector3(_ftX, _ftY, 0);
+				
+				_intCurrent += 1;
 			}
+			else
+			{
+				_goPlayer.GetComponent<PlayerStats>()._intSpawnPoint = 0;
+				_intCurrent = 0;
+			}
+
+			_ftTime = 0.0f;
 		}
 	}
 }
--- FYPJ/Assets/Scripts/Audio Scripts/SpeakerBeatMotion.cs
+++ FYPJ/Assets/Scripts/Audio Scripts/SpeakerBeatMotion.cs
@@ -2,27 +2,33 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
-using UnityEditor;
 
-public class DiscoBeatMotion : MonoBehaviour {
-	
+public class SpeakerBeatMotion : MonoBehaviour {
+
 	Transform _tfCamera;
 	Transform _tfParent;
 	Transform _tfThis;
     public float _intShiftRate;
 	public float _ftRange;
-	public Vector3 _vec3Area;
-	public Vector3 _vec3Shift;
+	float _ftX, _ftY, _ftZ;
+	int _intNumber;
+	Vector3 _vec3Area;
 
 	void Start() {
 		_tfThis = this.transform;
 		_tfParent = this.transform.parent.transform;
 		_tfCamera = Camera.main.transform;
-		_vec3Area = new Vector3(-0.5f, 0, 0);
+
+		_ftX = Random.Range(-1.2f, 1.2f);
+		_ftY = Random.Range(-1.2f, 1.2f);
+		_ftZ = 1.2f;
     }
 
-	void Update () {		
-		_tfThis.Rotate(_tfThis.forward, 7.0f);
+	void Update () {
+		_vec3Area = (_tfCamera.position + _tfCamera.forward * 0.5f) + new Vector3(_ftX,
+													 							  _ftY,
+													 							  _ftZ);
+		_tfThis.Rotate(_tfThis.up, 7.0f);
 		
 		BeatMotion();
 		TransitBeat();
@@ -33,11 +39,9 @@ public class DiscoBeatMotion : MonoBehaviour {
 	}
 
 	void TransitBeat() {
-		Vector3 _vec3Heading = _vec3Area + _vec3Shift + _tfCamera.position + _tfCamera.forward * 0.5f - _tfThis.position;
+		Vector3 _vec3Heading = _vec3Area - _tfThis.position;
 		if (!(_vec3Heading.sqrMagnitude < 0.1f * 0.1f))
-			_tfThis.position = Vector3.MoveTowards(_tfThis.position,
-												   _vec3Area + _vec3Shift + _tfCamera.position + _tfCamera.forward * 0.5f,
-			 									   0.075f);
+			_tfThis.position = Vector3.MoveTowards(_tfThis.position, _vec3Area, 0.1f);
 		else
 			Destroy(_tfThis.gameObject);
 	}
