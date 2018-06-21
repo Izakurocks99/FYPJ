8bbcea709060b018fda2bb205824f1d78e501063
Patrik Broms <patrik.broms95@gmail.com>
2018-06-21 11:34:32 +0800

    merge

--- FYPJ/Assets/LightController.cs
+++ FYPJ/Assets/LightController.cs
@@ -7,25 +7,30 @@ public class LightController : MonoBehaviour {
     // Use this for initialization
     [SerializeField]
     Shader sha = null;
-
+    [SerializeField]
+    Shader GlassShader = null;
     Light light = null;
     Material m = null;
     GameObject parent = null;
+    Material GlassMat = null;
 	void Start () {
         Debug.Assert(sha);
+        Debug.Assert(GlassShader);
         parent = this.transform.parent.gameObject;
         Debug.Assert(parent);
         light = parent.GetComponentInChildren<Light>();
         Debug.Assert(light);
         m = new Material(sha);
         this.GetComponentInChildren<MeshRenderer>().material = m;
-
+        GlassMat = new Material(GlassShader);
+        parent.GetComponent<MeshRenderer>().material = GlassMat;
     }
 
     // Update is called once per frame
     void Update () {
         Vector4 color = light.color;
-        m.SetVector("_ColorScales",new Vector3(color.x,color.y,color.z ));	
+        m.SetVector("_ColorScales",new Vector3(color.x,color.y,color.z ));
+        GlassMat.SetVector("_Color",color);
 	}
 }
 
