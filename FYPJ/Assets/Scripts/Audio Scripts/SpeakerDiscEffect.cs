51e6b5fa0ad023ef1f13cbacd6b5068dbf4d8be0
LYoshiiro <lyoshiiro@gmail.com>
2018-06-26 17:56:32 +0800

    Speaker disc effects
    Addition:
     - Speaker Disc Effect.

---
+++ FYPJ/Assets/Scripts/Audio Scripts/SpeakerDiscEffect.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakerDiscEffect : MonoBehaviour {
+
+	public GameObject[] _goDisc;
+	public GameObject _goAudio;
+	float[] _ftBandBuffer;
+	float _ftBandMax;
+
+	void Start () {
+		_ftBandBuffer = new float[AudioSampler._ftBandbuffer.Length];
+	}
+	
+	void Update () {
+		if (_goAudio.GetComponent<AudioSource>().clip != null &&
+			_goAudio.GetComponent<AudioSource>().isPlaying == true)
+		{
+			GetTotal();
+			DiscScaling();
+		}
+	}
+
+	void DiscScaling() {
+		for (int i = 0; i < _goDisc.Length; i++) {
+			if (((_ftBandMax / 45) * 0.25f) < 0.2f)
+				_goDisc[i].transform.localScale = new Vector3(1, 1, 1) +
+												  new Vector3(1, 1, 1) * ((_ftBandMax / 45) * 0.25f);
+
+			Debug.Log(_goDisc[i].transform.localScale + " || " + i);
+		}
+	}
+
+	void GetTotal() {
+		_ftBandBuffer = AudioSampler._ftBandbuffer;
+		_ftBandMax = 0.0f;
+
+		for (int i = 0; i < _ftBandBuffer.Length; i++)
+			_ftBandMax += _ftBandBuffer[i];
+	}
+}
