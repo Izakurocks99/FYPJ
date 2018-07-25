a0f5eb212e36814f97d1fb78200faa1dc192084e
LYoshiiro <lyoshiiro@gmail.com>
2018-07-25 16:57:03 +0800

    Fixed up some prefabs that had some wrong settinfs and added the in the linear game mode.
    Fixes:
     - Prefabs which had wrong setting for some particles were fixed.
    Changes:
     - Shift the old markers to fix in with the new system.
    Additions:
     - Added in the linear gae mode.

--- FYPJ/Assets/Scripts/Audio Scripts/AudioVisualiser.cs
+++ FYPJ/Assets/Scripts/Audio Scripts/AudioVisualiser.cs
@@ -8,78 +8,85 @@ public class AudioVisualiser : MonoBehaviour {
 
     public GameObject _goRParent;
     public GameObject _goLParent;
-    public static float[] _ftRSamplesbands;
-    public static float[] _ftLSamplesbands;
-    public static float[] _ftRSamplesbuffer;
-    public static float[] _ftLSamplesbuffer;
-    float[] _ftRBufferDecrease;
-    float[] _ftLBufferDecrease;
+    public GameObject _goCenter;
+    GameObject[] _goRArray;
+    GameObject[] _goLArray;
+    public static float[] _ftSSamplesBuffer;
+    public static float _ftCSampleBuffer;
+    public float _ftScale;
+    float[] _ftSBufferDecrease;
+    float _ftCBufferDecrease;
 
     void Start() {
-        _ftRSamplesbands = new float[_goRParent.transform.childCount];
-        _ftLSamplesbands = new float[_goLParent.transform.childCount];
+        _ftSSamplesBuffer = new float[_goLParent.transform.childCount];
+        _ftSBufferDecrease = new float[_goRParent.transform.childCount];
+        _ftCBufferDecrease = 0.0f;
 
-        _ftRSamplesbuffer = new float[_goRParent.transform.childCount];
-        _ftLSamplesbuffer = new float[_goLParent.transform.childCount];
-        
-        _ftRBufferDecrease = new float[_goRParent.transform.childCount];
-        _ftLBufferDecrease = new float[_goLParent.transform.childCount];
+        _goRArray = new GameObject[_goRParent.transform.childCount];
+        _goLArray = new GameObject[_goLParent.transform.childCount];
+
+        for (int _intR =  0; _intR < _goRArray.Length; _intR++)
+            _goRArray[_intR] = _goRParent.transform.GetChild(_intR).gameObject;
+        for (int _intL =  0; _intL < _goLArray.Length; _intL++)
+            _goLArray[_intL] = _goLParent.transform.GetChild(_intL).gameObject;
     }
 
     void Update() {
         if (this.GetComponent<AudioSource>().isPlaying == true &&
             this.GetComponent<AudioSource>().clip != null) {
 
-            UpdateSamples();
             ScaleBuffer();
             ScaleVisualise();
         }
     }
 
-    void UpdateSamples() {
-        for (int i = 0; i < _ftRSamplesbands.Length; i++)
-            _ftRSamplesbands[i] = AudioSampler._ftSamples[i];
-        for (int j = 1; j < _ftRSamplesbands.Length; j++)
-            _ftLSamplesbands[j - 1] = AudioSampler._ftSamples[j];
-    }
-
     void ScaleBuffer() {
-        for (int k = 0; k < _goRParent.transform.childCount; k++) {
-            if (_ftRSamplesbands[k] > _ftRSamplesbuffer[k]) {
-                _ftRSamplesbuffer[k] = _ftRSamplesbands[k];
-                _ftRBufferDecrease[k] = 0.005f;
-            }
-            else if (_ftRSamplesbands[k] < _ftRSamplesbuffer[k]) {
-                _ftRSamplesbuffer[k] -= _ftRBufferDecrease[k];
-                _ftRBufferDecrease[k] *= 1.2f;
-                if (_ftRSamplesbuffer[k] < 0.04f)
-                    _ftRSamplesbuffer[k] = 0.04f;
-            }
+        if (AudioSampler._ftSamples[0] > _ftCSampleBuffer) {
+            _ftCSampleBuffer = AudioSampler._ftSamples[0];
+            _ftCBufferDecrease = 0.002f;
         }
-        for (int q = 0; q < _goRParent.transform.childCount; q++) {
-            if (_ftRSamplesbands[q] > _ftRSamplesbuffer[q]) {
-                _ftRSamplesbuffer[q] = _ftRSamplesbands[q];
-                _ftRBufferDecrease[q] = 0.005f;
+        else if (AudioSampler._ftSamples[0] < _ftCSampleBuffer) {
+            _ftCSampleBuffer -= _ftCBufferDecrease;
+            _ftCBufferDecrease *= 1.2f;
+            if (_ftCSampleBuffer < 0.004f)
+                _ftCSampleBuffer = 0.004f;
+        }
+
+        for (int i = 0; i < _ftSSamplesBuffer.Length; i++) {
+            if (AudioSampler._ftSamples[i + 1] > _ftSSamplesBuffer[i]) {
+                _ftSSamplesBuffer[i] = AudioSampler._ftSamples[i + 1];
+                _ftSBufferDecrease[i] = 0.002f;
             }
-            else if (_ftRSamplesbands[q] < _ftRSamplesbuffer[q]) {
-                _ftRSamplesbuffer[q] -= _ftRBufferDecrease[q];
-                _ftRBufferDecrease[q] *= 1.2f;
-                if (_ftRSamplesbuffer[q] < 0.04f)
-                    _ftRSamplesbuffer[q] = 0.04f;
+            else if (AudioSampler._ftSamples[i + 1] < _ftSSamplesBuffer[i]) {
+                _ftSSamplesBuffer[i] -= _ftSBufferDecrease[i];
+                _ftSBufferDecrease[i] *= 1.2f;
+                if (_ftSSamplesBuffer[i] < 0.004f)
+                    _ftSSamplesBuffer[i] = 0.004f;
             }
         }
     }
 
     void ScaleVisualise() {
-        for (int i = 0; i < _goRParent.transform.childCount; i++) {
-            if (_goRParent.transform.GetChild(i) != null) {
-                _goRParent.transform.GetChild(i).transform.localScale = new Vector3(2.0f, _ftRSamplesbands[i] * 500.0f + 10.0f, 2.0f);
-            }
-        }
-        for (int j = 1; j < _goLParent.transform.childCount; j++) {
-            if (_goLParent.transform.GetChild(j - 1) != null) {
-                _goLParent.transform.GetChild(j - 1).transform.localScale = new Vector3(2.0f, _ftLSamplesbands[j] * 500.0f + 10.0f, 2.0f);
-            }
+        // _goCenter.transform.localScale = new Vector3(_goCenter.transform.localScale.x * _ftScale,
+        //                                             (AudioSampler._ftSamples[0] * 500.0f) + _ftScale * 3f,
+        //                                              _goCenter.transform.localScale.z * _ftScale);
+        _goCenter.transform.localScale = new Vector3(_goCenter.transform.localScale.x * _ftScale,
+                                                    (_ftCSampleBuffer * 500.0f) + _ftScale * 7.0f,
+                                                     _goCenter.transform.localScale.z * _ftScale);
+        
+        for (int i = 0; i < _goRArray.Length; i++) {
+            // _goRArray[i].transform.localScale = new Vector3(_goRArray[i].transform.localScale.x * _ftScale,
+            //                                                (AudioSampler._ftSamples[i + 1] * 500.0f) + _ftScale * 3f,
+            //                                                 _goRArray[i].transform.localScale.z * _ftScale);
+            // _goLArray[i].transform.localScale = new Vector3(_goLArray[i].transform.localScale.x * _ftScale,
+            //                                                (AudioSampler._ftSamples[i + 1] * 500.0f) + _ftScale * 3f,
+            //                                                 _goLArray[i].transform.localScale.z * _ftScale);
+            _goRArray[i].transform.localScale = new Vector3(_goRArray[i].transform.localScale.x * _ftScale,
+                                                           (_ftSSamplesBuffer[i] * 500.0f) + _ftScale * 7.0f,
+                                                            _goRArray[i].transform.localScale.z * _ftScale);
+            _goLArray[i].transform.localScale = new Vector3(_goLArray[i].transform.localScale.x * _ftScale,
+                                                           (_ftSSamplesBuffer[i] * 500.0f) + _ftScale * 7.0f,
+                                                            _goLArray[i].transform.localScale.z * _ftScale);
         }
     }
 }
