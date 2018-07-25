986d8bb80cb0f81ce3279bc63192498744a84f62
Isaac Choo <isaacchoo_jl@hotmail.com>
2018-07-25 17:06:37 +0800

    menustuff

---
+++ FYPJ/Assets/Scripts/IsaacSceneScripts/NumOfColorsScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumOfColorsScript : MonoBehaviour {
+
+    [SerializeField]
+    Button fourColors;
+    [SerializeField]
+    Button twoColors;
+
+    // Use this for initialization
+    void Start () {
+        if (PlayerPrefs.GetInt("dualcolors") == 1)
+            twoColors.gameObject.SetActive(false);
+        else
+            fourColors.gameObject.SetActive(false);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
