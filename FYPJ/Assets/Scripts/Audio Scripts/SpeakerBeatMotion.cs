92c52565b9a0110bf17ed7fdc42fe14eeaf5475b
LYoshiiro <lyoshiiro@gmail.com>
2018-07-27 13:46:56 +0800

    `

--- FYPJ/Assets/Scripts/Audio Scripts/SpeakerBeatMotion.cs
+++ FYPJ/Assets/Scripts/Audio Scripts/SpeakerBeatMotion.cs
@@ -10,10 +10,11 @@ public class SpeakerBeatMotion : MonoBehaviour {
 	Transform _tfThis;
     public float _intShiftRate;
     public float _ftTimeToTravel;
-    float _tfTime;
+    float _ftTime;
 	int _intNumber;
 	Vector3 _vec3Shift;
 	Vector3 _vec3Area;
+    ControlCalibrationScript calibration;
 
     //void Start() {
     //	_tfThis = this.transform;
@@ -45,19 +46,22 @@ public class SpeakerBeatMotion : MonoBehaviour {
         _tfThis = this.transform;
         //_tfParent = this.transform.parent.transform;
         _tfCamera = Camera.main.transform;
-        _vec3Area = (_tfCamera.position + _tfCamera.forward * 0.5f);
         _vec3Shift = new Vector3(Random.Range(-0.125f, 0.125f),
                                  Random.Range(-0.125f, 0.125f),
                                  0.0f);
-        _vec3Area += _vec3Shift;
+        calibration = FindObjectOfType<ControlCalibrationScript>();
+
+        _vec3Area = (_tfCamera.position + (_tfCamera.forward * 0.5f)) + _vec3Shift;
     }
 
     void Update () {
 		// _tfThis.Rotate(_tfThis.forward, 7.0f);
+        Debug.Log("Transiting");
 		
 		BeatMotion();
-        if (!_die)
+        if (!_die) {
             TransitBeat();
+        }
         else
             Dissolve();
     }
@@ -67,17 +71,11 @@ public class SpeakerBeatMotion : MonoBehaviour {
 	}
 
 	void TransitBeat() {
-        _tfTime += Time.deltaTime / _ftTimeToTravel;
-		Vector3 _vec3Heading = _vec3Area - _tfThis.position;
-
+        _ftTime += Time.deltaTime / _ftTimeToTravel;
+        Vector3 _vec3Heading = _vec3Area - _tfThis.position;
         if (!(_vec3Heading.sqrMagnitude < 0.1f * 0.1f))
-            _tfThis.position = Vector3.Lerp(_tfThis.transform.position, _vec3Area, _tfTime);
-        else if (_vec3Heading != Vector3.zero && _vec3Heading.z >= -0.15f) {
-            _tfTime = 0.0f;
-            OnReturn();
-        }
+            _tfThis.position = Vector3.Lerp(_tfThis.transform.position, _vec3Area, _ftTime);
         else {
-            _tfTime = 0.0f;
             OnReturn();
         }
     }
