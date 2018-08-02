242758122d86cf5594ca5d6be4250bb690ccdabe
Isaac Choo <isaacchoo_jl@hotmail.com>
2018-08-02 11:50:16 +0800

    update

--- FYPJ/Assets/Scripts/Audio Scripts/AudioMotion.cs
+++ FYPJ/Assets/Scripts/Audio Scripts/AudioMotion.cs
@@ -154,7 +154,7 @@ public class AudioMotion : MonoBehaviour
 #else
             Destroy(_tfThis.gameObject);
 #endif
-            //PlayerStats.ModifyScore(-1);
+            _goPlayer.GetComponent<PlayerStats>().ModifyCombo(false);
         }
     }
 
