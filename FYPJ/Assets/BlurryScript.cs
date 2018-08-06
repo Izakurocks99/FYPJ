de244c461248c27216ea641ae62f58dadc0a7498
Pendergaster <patrik.broms95@gmail.com>
2018-08-06 12:53:25 +0800

    dome prefab and camera

---
+++ FYPJ/Assets/BlurryScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO do either blurr or somekind of lining for the object
+// world position?
+// model position?
+// movement ?
+// scale ?
+// emission ?
+//
+public class BlurryScript : MonoBehaviour {
+
+    //Material mat = null;    
+    [SerializeField] 
+    Shader sha = null;
+	// Use this for initialization
+    //[ImageEffectOpaque]
+	void OnEnable () {
+        this.GetComponent<Camera>().SetReplacementShader(sha,"Outlined"); // or outlined ?
+	}
+	void OnDisable () {
+        this.GetComponent<Camera>().ResetReplacementShader();
+	}
+
+}
